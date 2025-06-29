6d45a3d8a4a4be95cdbbd86d40064542f2cb5b25
Robyn Proffer <robynproffer@go.rmc.edu>
2025-06-29 11:35:13 -0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,23 @@
+import requests
+
+def getWeather(key):
+    ##getting latitude and longitude from a phone is unfeasible, however, let's pretend I did that
+    lat=40
+    lon=80
+    #get weather
+    weatherReport=requests.get("https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={key}") #oof it wants money for this
+    #format weather report
+    print(weatherReport.json())
+    hourlyWeather=weatherReport[hourly[weather[main]]]
+    weatherReport="In an hour, it will be " + hourlyWeather
+    return weatherReport
+
+def main(key):
+    weatherReport=getWeather(key)
+    #send to phone
+    requests.post('https://ntfy.sh/xweatgery', weatherReport)
+
+if __name__ == "__main__":
+    main()
+
+
